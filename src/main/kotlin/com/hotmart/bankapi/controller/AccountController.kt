cd13b5fe56254a0605b90ba8fbb4b760f34efee3
Luiz Almeida <luizorst@hotmail.com>
2021-05-18 14:42:11 -0300

    [REFACTOR] account controller

--- src/main/kotlin/com/hotmart/bankapi/controller/AccountController.kt
+++ src/main/kotlin/com/hotmart/bankapi/controller/AccountController.kt
@@ -1,42 +1,37 @@
 package com.hotmart.bankapi.controller
 
 import com.hotmart.bankapi.model.Account
-import com.hotmart.bankapi.repository.AccountRepository
+import com.hotmart.bankapi.service.AccountService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/accounts")
-class AccountController(private val repository: AccountRepository) {
+class AccountController(private val service: AccountService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody account: Account): Account = repository.save(account)
+    fun create(@RequestBody account: Account): Account = service.create(account)
 
     @GetMapping
-    fun getAll(): List<Account> = repository.findAll()
+    fun getAll(): List<Account> = service.getAll()
 
     @GetMapping("/{id}")
     fun getById(@PathVariable id: Long): ResponseEntity<Account> =
-        repository.findById(id).map {
+        service.getById(id).map {
             ResponseEntity.ok(it)
         }.orElse(ResponseEntity.notFound().build())
 
     @PutMapping("/{id}")
     fun update(@PathVariable id: Long, @RequestBody account: Account): ResponseEntity<Account> =
-        repository.findById(id).map {
-            val accountToUpdate = it.copy(
-                name = account.name, document = account.document, phone = account.phone
-            )
-            ResponseEntity.ok(repository.save(accountToUpdate))
+        service.update(id, account).map {
+            ResponseEntity.ok(it)
         }.orElse(ResponseEntity.notFound().build())
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long): ResponseEntity<Void> =
-        repository.findById(id).map {
-            repository.delete(it)
-            ResponseEntity<Void>(HttpStatus.OK)
-        }.orElse(ResponseEntity.notFound().build())
-
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> {
+        service.delete(id)
+        return ResponseEntity<Void>(HttpStatus.OK)
+    }
 }
--- src/main/kotlin/com/hotmart/bankapi/service/AccountService.kt
+++ src/main/kotlin/com/hotmart/bankapi/service/AccountService.kt
@@ -1,4 +1,20 @@
 package com.hotmart.bankapi.service
 
+import com.hotmart.bankapi.model.Account
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import java.util.*
+
 interface AccountService {
+
+    fun create(account: Account): Account
+
+    fun getAll(): List<Account>
+
+    fun getById(id: Long): Optional<Account>
+
+    fun update(id: Long, account: Account): Optional<Account>
+
+    fun delete(id: Long)
+
 }
